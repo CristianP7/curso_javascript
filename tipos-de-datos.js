87cce166c16ba54ac6fd362bef56bf7511f4765a
Cristian <paulocristianpereira@gmail.com>
2026-01-29 18:45:53 -0300

    Se agregaron tipos de datos en el archivo js

---
+++ tipos-de-datos.js
@@ -0,0 +1,37 @@
+// DATOS PRIMITIVOS
+
+// string
+let nombre = 'Cristian'
+
+// number
+let edad = 25
+
+// boolean
+let esMayorDeEdad = true //false
+
+//null
+let noHayValor = null //hay algo vacio
+
+// undefinded
+let noDefinido = undefined // si no hay un valor definido JS, determinará que no está definido          
+
+// symbol
+let simboloUnico = Symbol('unico') 
+
+// bigint
+
+let numeroGrande = 2n //2n numero grande de JS
+
+// DATOS COMPLEJOS
+
+// object
+let carro = { //se hacen con llaves
+    marca: 'Tesla',
+    modelo: 'Model S'
+}
+
+// array o listas
+let frutas = ['manzana', 'banano', 'uvas']
+
+// function 
+function saludar () {}
