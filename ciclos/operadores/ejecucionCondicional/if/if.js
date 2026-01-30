24f3c78fd431dacc8d8b01db61acaf37ac363f7b
Cristian <paulocristianpereira@gmail.com>
2026-01-29 21:44:11 -0300

    Se agregó archivo explicativo de cómo funciona el condicional if

---
+++ ciclos/operadores/ejecucionCondicional/if/if.js
@@ -0,0 +1,9 @@
+let nombre = 'Cristian'
+
+if (nombre === 'Cristian'){
+    console.log('Hola Cristian')
+} else if (nombre === 'Nico'){
+    console.log ('Hola Nico')
+} else {
+    console.log('Error')
+}
