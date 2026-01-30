46f0186d5c0c6a1f79456e0460dac4c1ea37fe5f
Cristian <paulocristianpereira@gmail.com>
2026-01-29 21:21:41 -0300

    se agregó archivos con operadores de comparación

---
+++ ciclos/operadores/index.js
@@ -0,0 +1,24 @@
+// Operadores de Comparación = van ayudar a generar una condición
+
+//  ==      =>
+//  ===     => para comparación este para válidar el tipo de dato y el valor
+//  !=      =>
+//  !==     => es diferente en el valor y en el tipo de datos
+//  > mayor
+//  < menor
+//  >= mayor o igual
+//  <= menor igual
+
+// EJEMPLOS
+
+const a = 10
+const b = 20
+const c = "10"
+
+a ==  b // false ya que a no es igual que b
+a === c // false ya que a no es igual que c ya que es el mismo valor, pero no el tipo de dato
+a != b // true ya que a es diferente a b
+a !== c // true ya que es diferente en el valor y tipo de dato
+a > b // false ya que a no es mayor que b
+a <= b // true ya que a es menor que b
+a > c // falso ya que 10 no es mayor que 10 
