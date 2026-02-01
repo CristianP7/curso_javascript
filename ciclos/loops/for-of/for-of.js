55c06b13cb1ac9839cdf20216e07bfa3c910c6ed
Cristian <paulocristianpereira@gmail.com>
2026-01-31 21:11:34 -0300

    se agregó ciclo for of y se explicó

---
+++ ciclos/loops/for-of/for-of.js
@@ -0,0 +1,7 @@
+// FOR OF es una forma de iterar diversos elementos (arrays o strings) ya que son una lista de algo
+
+let canasta = ["manzana", "pera", "naranja", "uva"]
+
+for (fruta of canasta) {
+    console.log(fruta)
+}
