1277037385db80b3c3cda604b08102f5c89f25cf
Cristian <paulocristianpereira@gmail.com>
2026-02-02 19:40:09 -0300

    se explicó el ciclo doWhile y se agregó archivos con casos de uso de los ciclos mencionados

---
+++ ciclos/loops/doWhile/doWhile.js
@@ -0,0 +1,8 @@
+// dowhile, ejecutará primero nuestro bloque de código y luego de ello evalua la condición
+
+let contador = 0 
+
+do {
+    console.log(contador)
+    contador++
+} while (contador < 10)
