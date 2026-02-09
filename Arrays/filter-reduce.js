1de2fbfd144b81eaac5cf0a942713e3923701acd
Cristian <paulocristianpereira@gmail.com>
2026-02-09 19:46:50 -0300

    Agregar ejemplo de uso del método filter en filter-reduce.js

--- Arrays/filter-reduce.js
+++ Arrays/filter-reduce.js
@@ -1,1 +1,6 @@
 // filter () Crea un nuevo array con elementos que cumplen una condición dada por una función
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+const evenNumbers = numbers.filter(number => number % 2 === 0) // % --> modulo
+
+console.log(numbers)
+console.log(evenNumbers)
