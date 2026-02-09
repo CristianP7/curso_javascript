0ac45ba073eaf1e2d2f25755b87b70b7f94c8d3d
Cristian <paulocristianpereira@gmail.com>
2026-02-09 20:02:39 -0300

    Agregar implementación de reduce para sumar elementos y contar frecuencia de palabras en filter-reduce.js

--- Arrays/filter-reduce.js
+++ Arrays/filter-reduce.js
@@ -4,3 +4,27 @@ const evenNumbers = numbers.filter(number => number % 2 === 0) // % --> modulo
 
 console.log(numbers)
 console.log(evenNumbers)
+
+// reduce() reduce un array a un solo valor
+
+//accumulator es como decir let accumulator = 0
+
+const numbersReduce = [1,2,3,4,5]
+const sum = numbersReduce.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+
+console.log(numbersReduce, '<====')
+console.log(sum)
+
+// caso 2  . reduce()
+
+const words = ["apple", "banana", "hello", "bye", "banana", "bye"]
+const wordsFrecuency = words.reduce((accumulator, currentValue) => {
+    if (accumulator[currentValue]) {
+        accumulator[currentValue]++
+    } else {
+        accumulator[currentValue] = 1
+    }
+    return accumulator
+}, {})
+
+console.log(wordsFrecuency , '<====')
