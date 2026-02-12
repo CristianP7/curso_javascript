615c66fc7a692700b47b8691a8039046ecae8035
Cristian <paulocristianpereira@gmail.com>
2026-02-11 21:14:16 -0300

    se creó archivo find para explicar diferencias

--- Arrays/find-findindex.js
+++ Arrays/find-findindex.js
@@ -4,3 +4,6 @@ const multipleOf5 = [5,10,15,20]
 const fisrtNumberGreaterThan10 = multipleOf5.find(number => number = 10)
 console.log(multipleOf5)
 console.log(fisrtNumberGreaterThan10)
+
+// finIndex() Devuelve el indice del primer elemento array que cumpla la condicion
+
