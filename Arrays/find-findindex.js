0dcb8c90d27153122d40e032f22c323f22364d33
Cristian <paulocristianpereira@gmail.com>
2026-02-11 21:13:02 -0300

    se creó archivo para explicar metodos find y findIndex

---
+++ Arrays/find-findindex.js
@@ -0,0 +1,6 @@
+// FIND () Devuelve el primer elemento que cumpla con una condición de una función proporcionada
+
+const multipleOf5 = [5,10,15,20]
+const fisrtNumberGreaterThan10 = multipleOf5.find(number => number = 10)
+console.log(multipleOf5)
+console.log(fisrtNumberGreaterThan10)
