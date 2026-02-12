9bf7da414c1ec20b7b5d2c948d0459e64c49235e
Cristian <paulocristianpereira@gmail.com>
2026-02-12 18:38:48 -0300

    se agregó explicación teorica sobre la estructura de objetos en JavaScript

---
+++ objetos/index.js
@@ -0,0 +1,14 @@
+// PROGRAMACIÓN ORIENTADA A OBEJTOS
+
+/* objeto: es una esctructura de datos que ayuda a guardar valores
+
+key / value
+
+objeto{
+    propiedad: valor,
+    porpiedad: valor,
+    propiedad: valor
+}
+
+*/ 
+
