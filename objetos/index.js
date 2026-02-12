250d3305507de414cd6002f12d6801395fcb478e
Cristian <paulocristianpereira@gmail.com>
2026-02-12 18:44:10 -0300

    se agregó un objeto 'persona' con propiedades y un método para saludar

--- objetos/index.js
+++ objetos/index.js
@@ -10,5 +10,18 @@ objeto{
     propiedad: valor
 }
 
+Metodos: son funciones que están dentro de los objetos
+
 */ 
 
+const persona = {
+    nombre: 'Cristian',
+    edad: 25,
+    direccion: {
+        calle: 'Serrano 266',
+        ciudad: 'Santiago'
+    },
+    saludar () {
+        console.log(`Hola, mi nombre es ${persona.nombre}`)
+    }
+}
