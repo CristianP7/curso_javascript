1db5b9c410a0e969f44fea664425f0284c5c5322
Cristian <paulocristianpereira@gmail.com>
2026-02-14 21:19:56 -0300

    Agrega implementación de clases y herencia con ejemplos de Animal y Perro en 'prototipos-y-herencia.js'

--- objetos/prototipos-y-herencia.js
+++ objetos/prototipos-y-herencia.js
@@ -1,3 +1,4 @@
+
 // Prototipos y Herencia en JavaScript
 
 // En JavaScript, los objetos pueden heredar propiedades y métodos de otros objetos a través de prototipos. Cada objeto tiene un enlace interno a otro objeto llamado su prototipo. Cuando intentas acceder a una propiedad o método de un objeto, JavaScript primero busca esa propiedad o método en el propio objeto. Si no lo encuentra, busca en el prototipo del objeto, y así sucesivamente hasta llegar al final de la cadena de prototipos.
@@ -5,3 +6,42 @@
 // Ejemplo de prototipos y herencia:
 
 // Definimos un constructor para crear objetos de tipo Persona
+
+class Animal {
+    constructor (nombre, tipo){
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+    emitirSonido() {
+        console.log("El animal emite un sonido")
+    }
+}
+//  extends extiende la clase de aninal a la nueva creada de perro para usar sus atributos
+
+/* 
+
+super() es una función especial en JavaScript que se utiliza en clases que heredan de otras clases.
+Llama al constructor de la clase padre (Animal) para inicializar las propiedades heredadas.
+Debe llamarse antes de usar "this" en el constructor de la clase derivada.
+En este caso, super(nombre, tipo) inicializa los atributos nombre y tipo de la clase Animal.
+  
+*/
+
+class Perro extends Animal{
+    constructor (nombre, tipo, raza){
+        super(nombre, tipo)
+        this.raza = raza
+    }
+    emitirSonido() {
+        console.log("El perro ladra")
+    }
+    correr() {
+        console.log(`${this.nombre} corre alegremente`)
+    }
+}
+
+const perro1 = new Perro("Balto", "Perro", "Alaskan Malamute")
+console.log (perro1)
+
+perro1.correr()
+perro1.emitirSonido() // llama este metodo ya que sobreescribimos el método ya que extendimos la clase
