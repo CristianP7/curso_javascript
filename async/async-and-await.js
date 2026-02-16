921085e98b49fb2974ad1ee841e9dcea4b5ce7c6
Cristian <paulocristianpereira@gmail.com>
2026-02-16 20:23:44 -0300

    Actualizar función 'fetchData' para imprimir todos los datos de la API y agregar nueva función 'fetchNewData' para obtener múltiples recursos

--- async/async-and-await.js
+++ async/async-and-await.js
@@ -26,13 +26,31 @@ async function fetchData() {
     try {
         let response = await fetch("https://rickandmortyapi.com/api/character")
         let data = await response.json();
-        console.log(data.results)
+        console.log(data)
     } catch (error) {
         console.log(error)
     }
 }
 
-fetchData()
+const urls = [
+    "https://rickandmortyapi.com/api/character",
+    "https://rickandmortyapi.com/api/location",
+    "https://rickandmortyapi.com/api/episode",
+]
+
+async function fetchNewData (){
+    try {
+        for await (let url of urls) {
+            let response = await fetch(url)
+            let data = await response.json()
+            console.log(data)
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+fetchNewData()
 
 /*
 
