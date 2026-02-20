38389b5361cabb99efad973bf3f0fbffb9978c89
Cristian <paulocristianpereira@gmail.com>
2026-02-20 19:03:40 -0300

    Agregar ejemplos de métodos adicionales para manipulación de cadenas en JavaScript

--- Curso-Oscar_Barajas/04-metodos-de-strings/strings.js
+++ Curso-Oscar_Barajas/04-metodos-de-strings/strings.js
@@ -19,7 +19,40 @@ console.log(texto2.substring(0, 4)); //Hola
 
 // Split(separador): Divide una cadena en un array de subcadenas utilizando un separador.
 
-const texto3= 'linea1 linea2 linea3';
-const lineas = texto3.split(' ');
+const texto3= 'linea1, linea2, linea3';
+const lineas = texto3.split(', ');
 console.log(lineas); //['linea1', 'linea2', 'linea3']
 
+// trim(), trimStart(), trimEnd(): Elimina espacios en blanco al inicio y/o al final de una cadena.
+
+const texto4 = '   Hola Mundo   ';
+console.log(texto4.trim()); //Hola Mundo
+console.log(texto4.trimStart()); //Hola Mundo   
+console.log(texto4.trimEnd()); //   Hola Mundo 
+
+// toUpperCase() y toLowerCase(): Convierte una cadena a mayúsculas o minúsculas. (Util para normalizar texto en formulario o comparaciones)
+
+const texto5 = 'Hola Mundo';
+console.log(texto5.toUpperCase()); //HOLA MUNDO
+console.log(texto5.toLowerCase()); //hola mundo
+
+// includes(subcadena): Verifica si una cadena contiene una subcadena específica y devuelve true o false.
+// mayusculas y minusculas importan, si quieres ignorar eso puedes usar toLowerCase() o toUpperCase() antes de comparar.
+
+const contenido = 'Aprende JavaScript desde cero';
+console.log(contenido.includes('Aprende')); //true
+console.log(contenido.includes('Python')); //false
+
+//startsWith(subcadena) y endsWith(subcadena): Verifican si una cadena comienza o termina con una subcadena específica.
+
+const saludo = 'archivo.md';
+console.log(saludo.startsWith('archivo')); //true
+console.log(saludo.endsWith('.md')); //true
+
+// replace() y replaceAll(): Reemplazan una subcadena por otra. replace() reemplaza solo la primera ocurrencia, mientras que replaceAll() reemplaza todas las ocurrencias.
+
+const texto6 = 'Hola JavaScript, Hola Cristian';
+console.log(texto6.replace('JavaScript', 'Carla')); //Hola Carla, Hola Cristian
+console.log(texto6.replace('Cristian', 'C++')); //Hola JavaScript, Hola C++
+console.log(texto6.replaceAll('Hola', 'Hi')); //Hi JavaScript, Hi Cristian
+console.log(texto6); //Hola JavaScript, Hola Cristian (El texto original no se modifica, replace y replaceAll devuelven una nueva cadena)
