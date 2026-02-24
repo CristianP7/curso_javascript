fe2626042c247d737366e22d78c40574d395ee30
Cristian <paulocristianpereira@gmail.com>
2026-02-24 19:27:40 -0300

    Agregar comentarios explicativos sobre la estructura básica de un switch en 'switch.js'

--- Curso-Oscar_Barajas/09-switch/switch.js
+++ Curso-Oscar_Barajas/09-switch/switch.js
@@ -2,6 +2,22 @@
 
 // Es una alternativa a usar múltiples if-else
 
+// La estructura básica de un switch es la siguiente:
+
+/*
+switch (expresión) {
+    case valor1:
+        // código a ejecutar si la expresión es igual a valor1
+        break;
+    case valor2:
+        // código a ejecutar si la expresión es igual a valor2
+        break;
+    default:
+        // código a ejecutar si la expresión no coincide con ningún valor
+}
+
+*/
+
 // Sintaxis:
 
 const dia = 'miercoles';
