273dc22f241a80d17c627e6df42d84efdef9982d
Cristian <paulocristianpereira@gmail.com>
2026-02-25 18:57:59 -0300

    Refactor ejemplo function to improve variable scope demonstration and logging

--- Curso-Oscar_Barajas/12-scope/scope.js
+++ Curso-Oscar_Barajas/12-scope/scope.js
@@ -11,12 +11,19 @@ function ejemplo() {
     const funcion = 'Soy de función';
 
     if (true) {
-        const bloque = 'Soy de Bloque'
-        console.log(bloque)
+        const bloque = 'Soy de Bloque';
+    console.log(`Bloque - Función: ${funcion}`);
+    console.log(`Bloque -Bloque: ${bloque}`);
+    console.log(`Bloque - Global: ${global}`); 
     }
-    console.log(funcion)
+    console.log(`Función: ${funcion}`);
+    // console.log(`Bloque: ${bloque}`); //No llega hasta acá porque bloque es de bloque y no de función y da error porque no existe
+    console.log(`Global: ${global}`); 
 }
 
-console.log(global)
+    console.log(ejemplo());
+    console.log(`Global - Función: ${funcion}`);
+    console.log(`Global - Bloque: ${bloque}`);
+    console.log(`Global - Global: ${global}`); 
 
 
