136a0b630669ced46aa33d80aab67c90683960da
Cristian <paulocristianpereira@gmail.com>
2026-02-26 20:11:53 -0300

    Agregar ejemplos de destructuración y spread operator en objetos

--- Curso-Oscar_Barajas/15-objetos/objetos.js
+++ Curso-Oscar_Barajas/15-objetos/objetos.js
@@ -19,3 +19,39 @@ console.log(nota[campo])
 
 console.log(nota.author?.name) // ? = Opcional Chaining
 
+// Destructuración
+
+const nota2 = {
+    id: 1,
+    title: 'Mi primera nota',
+    content: 'Contenido de la nota',
+    createdAt: Date.now(),
+}
+
+const id = nota2.id;
+const title = nota2.title;
+console.log(id, title)
+
+const {title: titulo, content} = nota2; // Destructuración de objetos, title: titulo = renombrar la variable title a titulo
+console.log(titulo, content)
+
+// Destructurar elementos es muy común a la hora de ver codigo, ya que nos permite acceder a las propiedades de un objeto de una manera más sencilla y legible. Además, también podemos usar la destructuración para asignar valores a variables de una manera más rápida y eficiente.
+
+
+// Spread operator
+
+const nota3 = {id: 2, title: 'Hola'};
+const data = { esAdmin:true};
+
+const copia = { ...nota3}; 
+
+console.log(nota3)
+console.log(copia)
+
+const notaActualizada = {
+    ...nota3,
+    ...data,
+    edad:18
+}
+
+console.log(notaActualizada)
