7c6e0b9f48a1724cebe9567106cbec47dbf0baae
Cristian <paulocristianpereira@gmail.com>
2026-04-15 20:14:55 -0400

    Agregar archivos app.js, index.html y styles.css para implementar la funcionalidad de visualización de planes

---
+++ Curso-Oscar_Barajas/DOM/practica-simple-cristian/app.js
@@ -0,0 +1,35 @@
+const planes = [
+    { id: 1, tipo: "básico", precio: 5},
+    { id: 2, tipo: "pro", precio: 50},
+    { id: 3, tipo: "premiun", precio: 500},
+
+];
+
+function crearPlanes(plan) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.dataset.id = plan.id;
+
+    const tipo = document.createElement("h2");
+    tipo.textContent = plan.tipo;
+
+    const price = document.createElement("div");
+    price.classList.add("price");
+    price.textContent = "$" + plan.precio;
+
+    card.appendChild(tipo);
+    card.appendChild(price);
+
+    return card;
+}
+
+function renderizarPlanes(list){
+    const contenedor = document.querySelector(".container");
+
+    list.forEach((plan) => {
+        const el = crearPlanes(plan);
+        contenedor.appendChild(el);
+    });
+}
+
+renderizarPlanes(planes)
