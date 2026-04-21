7c61d1d1d498440f9a87001ef37f9c0459083f4b
Cristian <paulocristianpereira@gmail.com>
2026-04-21 15:58:33 -0400

    Refactor click handling logic in cristapp.js for improved readability

--- Curso-Oscar_Barajas/DOM/events-cristian/cristapp.js
+++ Curso-Oscar_Barajas/DOM/events-cristian/cristapp.js
@@ -4,16 +4,22 @@ const $reset = document.getElementById('reset');
 
 function manipulateClicks(){
     let clicks = 0;
-    
-    $click.addEventListener("click", () =>{
-        clicks += 1;
+
+    function render(){
         $counter.textContent = clicks;
-    })
+    }
+
+    $click.addEventListener("click", () =>{
+        clicks++;
+        render();
+    });
 
     $reset.addEventListener("click", () => {
         clicks = 0;
-        $counter.textContent = clicks;
-    })
+        render();
+    });
+
+    render();
 }
 
 manipulateClicks();
