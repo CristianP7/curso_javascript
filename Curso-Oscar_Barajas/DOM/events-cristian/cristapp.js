4077e6f024e6fbd3f91d41dfb5d67d8a12cf2cc9
Cristian <paulocristianpereira@gmail.com>
2026-04-21 16:12:43 -0400

    Agregar funcionalidad de cambio de color al pasar el mouse sobre el elemento en index.html y cristiapp.js

--- Curso-Oscar_Barajas/DOM/events-cristian/cristapp.js
+++ Curso-Oscar_Barajas/DOM/events-cristian/cristapp.js
@@ -1,6 +1,7 @@
 const $counter = document.getElementById('counter');
 const $click = document.getElementById('click');
 const $reset = document.getElementById('reset');
+const $box = document.getElementById("box");
 
 function manipulateClicks(){
     let clicks = 0;
@@ -21,5 +22,12 @@ function manipulateClicks(){
 
     render();
 }
+
+function changeColor(color){
+    $box.style.backgroundColor = color;
+}
+
+$box.addEventListener("mouseenter", () => changeColor("black"));
+$box.addEventListener("mouseleave", () => changeColor("#2A7B9B"));
 
 manipulateClicks();
