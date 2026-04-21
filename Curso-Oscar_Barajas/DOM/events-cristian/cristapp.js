795cffecf6f1e73829963a30955b6a3331877730
Cristian <paulocristianpereira@gmail.com>
2026-04-21 15:52:56 -0400

    Agregar funcionalidad para contar clics y reiniciar en cristiapp.js

--- Curso-Oscar_Barajas/DOM/events-cristian/cristapp.js
+++ Curso-Oscar_Barajas/DOM/events-cristian/cristapp.js
@@ -1,1 +1,19 @@
-const counter = document.getElementById('counter');
+const $counter = document.getElementById('counter');
+const $click = document.getElementById('click');
+const $reset = document.getElementById('reset');
+
+function manipulateClicks(){
+    let clicks = 0;
+    
+    $click.addEventListener("click", () =>{
+        clicks += 1;
+        $counter.textContent = clicks;
+    })
+
+    $reset.addEventListener("click", () => {
+        clicks = 0;
+        $counter.textContent = clicks;
+    })
+}
+
+manipulateClicks();
