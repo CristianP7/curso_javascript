4e7098128ad41b37bf8ba20ba30146aea576f6fc
Cristian <paulocristianpereira@gmail.com>
2026-04-27 20:47:00 -0400

    Add math module with PI constant and basic arithmetic functions

---
+++ Curso-Oscar_Barajas/17-modules.js/modules.js
@@ -0,0 +1,10 @@
+// ESM
+
+// reutilización, mantenibilidad y encapsulamiento
+// export e import
+
+import {PI, sumar, restar} from './math.js';
+
+console.log(PI);
+console.log(sumar(4,4));
+console.log(restar(4,2));
